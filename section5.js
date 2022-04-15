6c0f52b0c76ea1acbd6430079eac55434184fc53
hyojinjeong <hyojin.dev@gmail.com>
2022-04-15 15:35:46 +0900

    feat: modified section5.js

--- section5.js
+++ section5.js
@@ -55,25 +55,64 @@
 // let b=[3, 2, 5, 7, 8];
 // console.log(solution(a, b));
 
-function solution(arr1, arr2){
-    // 두 배열을 오름차순으로 정렬한다.
-    // 각각의 포인트에서 원소가 같을 때 까지 작은 포인트를 증가시킴
-    let answer=[];
-    arr1.sort((a, b) => a - b);
-    arr2.sort((a, b) => a - b);
-    let p1=p2=0;
-    while(p1<arr1.length && p2<arr2.length){
-        if(arr1[p1]===arr2[p2]){
-            answer.push(arr1[p1++]);
-            p2++;
-        }
-        else if(arr1[p1]<arr2[p2]) p1++
-        else p2++;
-    }
+// function solution(arr1, arr2){
+//     // 두 배열을 오름차순으로 정렬한다.
+//     // 각각의 포인트에서 원소가 같을 때 까지 작은 포인트를 증가시킴
+//     let answer=[];
+//     arr1.sort((a, b) => a - b);
+//     arr2.sort((a, b) => a - b);
+//     let p1=p2=0;
+//     while(p1<arr1.length && p2<arr2.length){
+//         if(arr1[p1]===arr2[p2]){
+//             answer.push(arr1[p1++]);
+//             p2++;
+//         }
+//         else if(arr1[p1]<arr2[p2]) p1++
+//         else p2++;
+//     }
+//     // 1 2 3 5 9 
+//     // 2 3 5 7 8 10 15 17
+//     return answer;
+// }
 
+// let a=[1, 3, 9, 5, 2];
+// let b=[3, 2, 5, 7, 8];
+// console.log(solution(a, b));
+
+// 연속부분수열1
+// function solution(m, arr) {
+//     let answer = 0,
+//         lt = 0,
+//         sum = 0;
+
+//     for (let rt = 0; rt < arr.length; rt++) {
+//         sum += arr[rt];
+//         if (sum === m) answer++;
+//         while (sum >= m) {
+//             sum -= arr[lt++];
+//             if(sum === m) answer++;
+//         }
+//     }
+//     return answer;
+// }
+
+
+// let a = [1, 2, 1, 3, 1, 1, 1, 2];
+// console.log(solution(6, a));
+
+// 연속부분수열2
+function solution(m, arr){
+    let answer=0, sum=0, lt=0;
+
+    for(let rt=0; rt<arr.length; rt++){
+        sum += arr[rt];
+        while(sum > m){
+            sum -= arr[lt++];
+        }
+        answer += (rt-lt+1);
+    }
     return answer;
 }
 
-let a=[1, 3, 9, 5, 2];
-let b=[3, 2, 5, 7, 8];
-console.log(solution(a, b));
+let a=[1, 3, 1, 2, 3];
+console.log(solution(5, a));
