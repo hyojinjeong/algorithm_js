a421226031d10d206fdaa2662c62fd6e50162cfd
hyojinjeong <hyojin.dev@gmail.com>
2024-01-15 01:02:59 +0900

    phone_num

--- pro_42576.js
+++ pro_42576.js
@@ -32,20 +32,25 @@
 //   return answer;
 // }
 
-function solution(participant, completion) {
-  let map = new Map();
+// function solution(participant, completion) {
+//   let map = new Map();
 
-  for (let i = 0; i < participant.length; i++) {
-    let p = participant[i];
-    let c = completion[i];
+//   for (let i = 0; i < participant.length; i++) {
+//     let p = participant[i];
+//     let c = completion[i];
 
-    map.set(p, ((map.get(p) || 0) + 1));
-    map.set(c, ((map.get(c) || 0) - 1));
-  }
-  for (let [k, v] of map) {
-    if (v > 0) {
-      return k
-    }
-  }
+//     map.set(p, ((map.get(p) || 0) + 1));
+//     map.set(c, ((map.get(c) || 0) - 1));
+//   }
+//   for (let [k, v] of map) {
+//     if (v > 0) {
+//       return k
+//     }
+//   }
+// }
+function solution(phone_book) {
+  phone_book.sort();
+  return !phone_book.some((v, i) => phone_book[i + 1]?.indexOf(v) === 0)
+  return result;
 }
 console.log(solution(['a', 'b', 'a', 'a'], ['a', 'a', 'a']));
